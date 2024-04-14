2fdfdeb2b805fbea7ef62afc861e89686af5da53
karan3267 <karanreddy3267@gmail.com>
2024-04-14 15:07:46 +0400

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+console.log(process.env.MONGO)
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-const app =express();
-const port="3000";
+const app = express();
+const port = "3000";
 
-app.listen(port,()=>{
-    console.log("server is running on port: "+port);
-})
+app.listen(port, () => {
+  console.log("server is running on port: " + port);
+});
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import {FaSearch} from 'react-icons/fa'
 import { Link } from 'react-router-dom';
 export default function Header() {
   return (
-    <header className="bg-slate-500">
+    <header className="bg-slate-500 text-slate-300">
       <div className=" flex items-center justify-between mx-4 p-3">
         <Link to={'/'}>
         <h1 className="text-xl md:text-3xl font-bold">
@@ -15,13 +15,13 @@ export default function Header() {
         </form>
         <ul className='sm:flex items-center gap-4 hidden text-xl '>
             <Link to={'/'}>
-            <li className='hover:text-slate-200'>Home</li>
+            <li className='hover:text-slate-50 hover:font-semibold'>Home</li>
             </Link>
             <Link to={'/about'}>
-            <li className='hover:text-slate-200'>About</li>
+            <li className='hover:text-slate-50 hover:font-semibold'>About</li>
             </Link>
             <Link to={'/sign-in'}>
-            <li className='hover:text-slate-200'>Signin</li>
+            <li className='hover:text-slate-50 hover:font-semibold'>Signin</li>
             </Link>
         </ul>
       </div>
