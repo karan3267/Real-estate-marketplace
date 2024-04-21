9cca21347c4eae67b88d9fbbab3c6b811ba9256d
karan3267 <karanreddy3267@gmail.com>
2024-04-21 10:43:43 +0400

    created get user listings api

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,3 +1,4 @@
+import Listing from "../models/listing.model.js";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.handler.js";
 import bcrypt from "bcryptjs";
@@ -50,5 +51,18 @@ export const signOut = (req, res, next) => {
     res.status(200).json("User has been signed out!");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async(req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const data = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(data);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your listings"));
   }
 };
